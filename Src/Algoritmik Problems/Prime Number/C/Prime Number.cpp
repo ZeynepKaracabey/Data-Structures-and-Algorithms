b38dc4660efd1834238fae9260bac7568d8c67dc
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-02-13 12:08:20 -0800

    Added Prime Number in C

---
+++ Src/Algoritmik Problems/Prime Number/C/Prime Number.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main () {
+	
+	int number;
+	
+	printf( "Enter a number:");
+	scanf( "%d",&number);
+	
+	if( number == 1 || number == 0 )
+		printf( "%d is not prime number",number );
+	
+	else if ( number == 2 )
+		printf( "%d is prime number",number);
+		
+	else {	
+			
+		for(int i =2; i < number ;i++) {
+			
+			if(number %i ==0)
+				printf( "%d is not prime number",number);
+			else if(number %i != 0 &&  number-1==i)
+				printf( "%d is prime number",number); 	
+		}
+	
+	}
+		
+
+	return 0;
+}
